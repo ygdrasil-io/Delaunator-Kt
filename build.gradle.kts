f3f50691375315d033e3d8e9c32244dd5ac6f4dd
Alexandre Mommers <none@ygdrasil.io>
2022-07-22 22:38:38 +0200

    try new workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ kotlin {
         repositories {
             maven {
                 name = "GitLab"
-                url = uri("https://gitlab.com/api/v4/projects/25805863/packages/maven/snapshot")
+                url = uri("https://gitlab.com/api/v4/projects/25805863/packages/maven")
                 credentials(HttpHeaderCredentials::class) {
                     name = "Deploy-Token"
                     value = System.getenv("TOKEN")
